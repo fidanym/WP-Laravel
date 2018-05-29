4b3bb2ade71e7d744fb4f1aacdf522ac8dbbb699
Fidan <fidanym95@gmail.com>
2018-05-29 17:15:58 +0200

    Updated post rendering and added comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,43 @@
 @extends ('layouts.master')
 
 @section('content')
-    <h1>A place to show the post.</h1>
+    <div class="col-md-8 blog-main">
+        <h1>
+            {{ $post->title }}
+        </h1>
+
+        {{ $post->body }}
+
+        <hr>
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach ($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                        </strong>
+                        {{ $comment->body }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+
+                @include ('layouts.errors')
+            </div>
+        </div>
+    </div>
 @endsection
